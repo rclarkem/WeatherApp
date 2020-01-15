cf05328f15a418415dabf301c465713cdfbc9a7a
Reina <r.clarkemitchell@gmail.com>
2020-01-14 23:18:11 -0500

    load component

---
+++ src/components/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loader(props) {
+	return (
+		<div className='ui active dimmer'>
+			<div className='ui big text loader'>Loading...</div>
+		</div>
+	)
+}
